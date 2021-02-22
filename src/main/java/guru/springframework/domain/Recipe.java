8104d2eb14fd7fa0ba065cea15dd3029641b6f05
Ryniu <marcin3001@10g.pl>
2021-02-22 22:25:18 +0100

    add UnitOfMeasure

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,11 +1,14 @@
 package guru.springframework.domain;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -21,9 +24,13 @@ public class Recipe {
 	private String url;
 	private String directions;
 
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	private Set<Ingredient> ingredients;
+	
 	@Lob
 	private Byte[] image;
 	
+	//TODO: check which side is the relationship owner
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
 
