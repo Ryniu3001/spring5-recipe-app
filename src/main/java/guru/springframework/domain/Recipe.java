494d4c76a678c9b81aceac873451db55a0ce6c74
Ryniu <marcin3001@10g.pl>
2021-02-22 22:46:15 +0100

    add enumerated value

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -29,6 +31,9 @@ public class Recipe {
 	
 	@Lob
 	private Byte[] image;
+
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
 	
 	//TODO: check which side is the relationship owner
 	@OneToOne(cascade = CascadeType.ALL)
@@ -105,6 +110,22 @@ public class Recipe {
 
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
 	}
 
 }
