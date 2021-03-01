ecfe108266eec1f63bd41a6fd45e681708ffd1b3
Ryniu <marcin3001@10g.pl>
2021-03-01 21:12:18 +0100

    Make bootstrap in one transaction

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -36,8 +38,8 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 	}
 
 	@Override
+	@Transactional
 	public void onApplicationEvent(ContextRefreshedEvent event) {
-		log.info("Bootstraping...");
 		recipeRepository.saveAll(getRecipes());
 		log.info("Bootstrap complited");
 	}
